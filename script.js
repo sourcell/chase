17f7631e19b3cff56c9e9e90e2ad2887afb7d48e
razzi <doktoragon4@gmail.com>
2016-08-06 17:50:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,160 @@
+/*
+==============================
+		CANVAS SETUP
+==============================
+*/
 var canvas = document.getElementById("map");
 var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth-6;
+canvas.height = window.innerHeight-6;
 
-var x;
-var y;
-var dist;
-var score = 0;
-var started = false;
-var fps = 60;
-var evadeOn = false;
+
+
+
+/*
+==============================
+	OBJECTS & VARIABLES
+==============================
+*/
+var Game = {
+	started: false,
+	FPS: 60
+}
 
 var map = {
 	x: 0,
 	y: 0,
-	width: 1000,
-	height: 563
+	width: canvas.width,
+	height: canvas.height,
+
+	clear: function(){
+		ctx.clearRect(this.x, this.y, this.width, this.height);
+	}
 };
 
 var enemy = {
 	x: map.width/2,
 	y: map.height/2,
 	size: 15,
-	speed: 4
+	speed: 5,
+
+	draw: function(){
+		ctx.fillStyle = "#F33";
+		ctx.strokeStyle = "#000";
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+	},
+
+	follow: function(unit){
+		if(Game.started == false){return}
+
+		if(unit.x > this.x){
+			if(unit.x - this.x > this.speed){
+				this.x += this.speed;
+			}
+		}else{
+			if(this.x - unit.x > this.speed){
+				this.x -= this.speed;
+			}	
+		}
+
+		if(unit.y > this.y){
+			if(unit.y - this.y > this.speed){
+				this.y += this.speed;
+			}	
+		}else{
+			if(this.y - unit.y > this.speed){
+				this.y -= this.speed;
+			}
+		}		
+	},
+
+	reset: function(){
+		this.x = map.width/2 - this.size;
+		this.y = map.height/2 - this.size;
+		this.size = 15;
+		this.speed = 5;
+	}
 };
 
 var player = {
 	x: 0,
 	y: 0,
 	size: 15,
-	speed: 10
+	speed: 10,
+	score: 0,
+	cheating: false,
+
+	draw: function(){
+		ctx.fillStyle = "#0A0";
+		ctx.strokeStyle = "#000";
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+	},
+
+	drawScore: function(x, y){
+		ctx.fillStyle = "#000";
+		ctx.font = "20px Trebuchet MS";
+		ctx.fillText("Score: " + this.score, x, y);
+	},
+
+	drawDist: function(unit, x, y){
+		ctx.fillStyle = "#000";
+		ctx.font = "20px Trebuchet MS";
+		ctx.fillText("Distance: " + getDist(this, unit) + "px", x, y);
+	},
+
+	hitCheck: function(unit){
+		if(getDist(this, unit) < this.size + unit.size){
+			alert("You have died. Score: " + this.score);
+			this.reset();
+			unit.reset();
+			Game.started = false;
+		}		
+	},
+
+	evade: function(unit){
+		if(getDist(this, unit) < this.size*2 + unit.size*2){
+			if(unit.x < map.width/2){
+				this.x = random(unit.x + unit.size*2 + this.size*2, map.width - this.size);
+			}else{
+				this.x = random(map.x + this.size, unit.x - unit.size*2 + this.size*2);
+			}
+
+			if(unit.y < map.height/2){
+				this.y = random(unit.y + unit.size*2 + this.size*2, map.height - this.size);
+			}else{
+				this.y = random(map.y + this.size, unit.y - unit.size*2 + this.size*2);
+			}
+		}		
+	},
+
+	reset: function(){
+		this.score = 0;
+	}
 };
 
-document.onmousemove = function(e){
+var msgbox = {
+	x: map.width / 2 - 200,
+	y: map.height / 2 - 100,
+	width: 400,
+	height: 200
+}
+
+
+
+
+/*
+==============================
+		EVENTS
+==============================
+*/
+
+document.addEventListener("mousemove", function(e){
 	if(e.clientX > map.width - player.size){
 		player.x = map.width - player.size;
 	}else{
@@ -41,42 +165,28 @@ document.onmousemove = function(e){
 		player.y = map.height - player.size;
 	}else{
 		player.y = e.clientY;
-	}
-}
+	}	
+});
 
-document.onkeydown = function(e){
+document.addEventListener("keydown", function(e){
 	if(e.keyCode == 45){
-		if(evadeOn == false){
-			evadeOn = true;
+		if(player.cheating){
+			player.cheating = false;
 		}else{
-			evadeOn = false;
+			player.cheating = true;
 		}
 	}
-}
+});
 
-document.onclick = function(e){
-	if(started == false){
-		started = true;
+document.addEventListener("mousedown", function(e){
+	if(Game.started){
+		Game.started = false;
+		enemy.reset();
+		player.reset();
+	}else{
+		Game.started = true;
 	}
-}
-
-function drawEnemy(){
-	ctx.fillStyle = "#F33";
-	ctx.strokeStyle = "#000";
-	ctx.beginPath();
-	ctx.arc(enemy.x, enemy.y, enemy.size, 0, 2*Math.PI);
-	ctx.fill();
-	ctx.stroke();
-}
-
-function drawPlayer(){
-	ctx.fillStyle = "#0A0";
-	ctx.strokeStyle = "#000";
-	ctx.beginPath();
-	ctx.arc(player.x, player.y, player.size, 0, 2*Math.PI);
-	ctx.fill();
-	ctx.stroke();
-}
+});
 
 function drawLine(){
 	ctx.strokeStyle = "#F00";
@@ -87,6 +197,9 @@ function drawLine(){
 }
 
 function getDist(a, b){
+	var x;
+	var y;
+
 	if(a.x > b.x){
 		x = a.x - b.x;
 	}else{
@@ -99,147 +212,52 @@ function getDist(a, b){
 		y = b.y - a.y;
 	}
 
-	dist = Math.round(Math.sqrt(x*x + y*y));
-	return dist;
-}
-
-function drawDist(){
-	ctx.fillStyle = "#000";
-	ctx.font = "20px Trebuchet MS";
-	ctx.fillText("Distance: " + getDist(player, enemy) + "px", map.x + 20, map.y + 30);
+	return Math.round(Math.sqrt(x*x + y*y));
 }
 
 function msg(){
 	ctx.fillStyle = "#FFF";
 	ctx.font = "20px Trebuchet MS";
-	ctx.fillText("Click to start!", map.x + 450, map.y + 30);
+	ctx.fillText("Click to start!", map.width / 2 - 70, map.y + 30);
 }
-
-function drawScore(){
-	ctx.fillStyle = "#000";
-	ctx.font = "20px Trebuchet MS";
-	ctx.fillText("Score: " + score, map.width - 100, map.y + 30);
-}
-
-function clear(){
-	ctx.clearRect(map.x, map.y, map.width, map.height);
-}
-
-function follow(followed, follower){
-	if(started == false){return}
-
-	if(followed.x > follower.x){
-		if(followed.x - follower.x > follower.speed){
-			follower.x += follower.speed;
-		}
-	}else{
-		if(follower.x - followed.x > follower.speed){
-			follower.x -= follower.speed;
-		}	
-	}
-
-	if(followed.y > follower.y){
-		if(followed.y - follower.y > follower.speed){
-			follower.y += follower.speed;
-		}	
-	}else{
-		if(follower.y - followed.y > follower.speed){
-			follower.y -= follower.speed;
-		}
-	}
-}
-
-function hitCheck(){
-	if(getDist(player, enemy) < player.size + enemy.size){
-		alert("You have died. Score: " + score);
-		score = 0;
-		enemy.x = map.width/2;
-		enemy.y = map.height/2;
-		enemy.size = 15;
-		started = false;
-	}
-}
-
-function incSpeed(){
-	if(score < 10){
-		enemy.speed = 4;
-		enemy.size = 15;
-	}else if(score >= 10 && score <= 20){
-		enemy.speed = 5;
-		enemy.size = 20;
-	}else if(score >= 21 && score <= 30){
-		enemy.speed = 6;
-		enemy.size = 25;
-	}else if(score >= 31 && score <= 40){
-		enemy.speed = 7;
-		enemy.size = 30;
-	}else if(score >= 41 && score <= 50){
-		enemy.speed = 8;
-		enemy.size = 35;
-	}else if(score >= 51 && score <= 60){
-		enemy.speed = 9;
-		enemy.size = 40;
-	}else if(score >= 61 && score <= 70){
-		enemy.speed = 10;
-		enemy.size = 45;
-	}else if(score >= 71 && score <= 80){
-		enemy.speed = 11;
-		enemy.size = 50;
-	}else if(score >= 81 && score <= 90){
-		enemy.speed = 12;
-		enemy.size = 55;
-	}else if(score >= 91 && score <= 100){
-		enemy.speed = 13;
-		enemy.size = 60;
-	}
-}
-
-/*function random(min, max){
-	var random = Math.round(Math.random()*max);
-
-	if(random < min){
-		random += min - random + Math.round(Math.random() * (max - min));
-		return random;
-	}else{
-		return random;
-	}
-}*/
 
 function random(min, max){
 	return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-function evade(who, what){
-	if(getDist(player, enemy) < player.size*2 + enemy.size*2){
-		if(enemy.x < map.width/2){
-			player.x = random(enemy.x + enemy.size*2 + player.size*2, map.width - player.size);
-		}else{
-			player.x = random(map.x + player.size, enemy.x - enemy.size*2 + player.size*2);
-		}
 
-		if(enemy.y < map.height/2){
-			player.y = random(enemy.y + enemy.size*2 + player.size*2, map.height - player.size);
-		}else{
-			player.y = random(map.y + player.size, enemy.y - enemy.size*2 + player.size*2);
-		}
-	}
-}
+
+
+/*
+==============================
+		INTERVALS
+==============================
+*/
+setInterval(function(){
+	map.clear();
+	
+	enemy.draw();
+	enemy.follow(player);
+	
+	player.draw();
+	player.drawDist(enemy, 20, 30);
+	player.drawScore(map.width - 150, 30);
+	player.hitCheck(enemy);
+
+	if(Game.started == false){msg();}
+	if(player.cheating == true){player.evade(enemy);}
+}, 1000/Game.FPS);
+
 
 setInterval(function(){
-	clear();
-	drawEnemy();
-	drawPlayer();
-	drawDist();
-	drawScore();
-	if(started == false){msg();}
-	follow(player, enemy);
-	if(evadeOn == true){evade(player, enemy);}
-	hitCheck();
-}, 1000/fps);
-
-setInterval(function(){
-	if(started){
-		score++;
+	if(Game.started){
+		player.score++;
 	}
-	incSpeed();
 }, 1000);
+
+setInterval(function(){
+	if(Game.started){
+		enemy.speed += 1;
+		enemy.size += 5;
+	}
+}, 10000);
